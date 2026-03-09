2a366701e96570f7d74d0792f62a239c96a99d81
Alicia200208 <aliciasilantoi@gmail.com>
2026-03-09 12:02:32 +0300

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from myapp import views
+urlpatterns=[
+    path('', views.user_list),
+    path('Add/', views.AddUser),
+    path('Edit/<id>', views.EditUser),
+    path('Delete/<eid>', views.DeleteUser),
+    path('View/<eid>', views.ViewUser),
+]
